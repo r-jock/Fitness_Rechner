0dc603c6acacaab0da5a9c55560c5773ca4b0453
r-jock <128504586+r-jock@users.noreply.github.com>
2023-03-21 18:50:31 +0100

    added first functions

--- Fitness-Rechner/Program.cs
+++ Fitness-Rechner/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int weight = Console.ReadLine("Gebe dein Gewicht in Kg ein:");
+            
+
+            // Shows the the Headline für the MainMenu.
+            void Show_Headline()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("### Calories Calculater & BMI Calculater ###");
+                Console.WriteLine("############################################");
+            }
+            // Shows the Headline for the Calories Calculater.
+            void Show_headline_menue()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("### Calories Calculater ###");
+                Console.WriteLine("###########################");
+            }
+            // Calculate the calories for Men.
+            double Calc_caloriesM(int weight)
+            {
+                Console.WriteLine("Dein Leistungsumsatz ist: ");
+                return 1.1 * weight * 24;
+            }
+            // Calculate the calories for Woman.
+            double Calc_caloriesW(int weight)
+            {
+                Console.WriteLine("Dein Leistungsumsatz ist: ");
+                return 0.9 * weight * 24;
+            }
         }
+
     }
 }
